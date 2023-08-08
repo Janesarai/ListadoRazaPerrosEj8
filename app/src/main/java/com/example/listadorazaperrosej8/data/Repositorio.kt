1ba968c82010384f30d48edb10a5828eecde6b79
Janesarai <saraipalaciosjane@gmail.com>
2023-08-07 22:05:13 -0400

    repositorio ok

---
+++ app/src/main/java/com/example/listadorazaperrosej8/data/Repositorio.kt
@@ -0,0 +1,20 @@
+package com.example.listadorazaperrosej8.data
+
+import com.example.listadorazaperrosej8.data.local.RazaDao
+import com.example.listadorazaperrosej8.data.local.RazaEntity
+import com.example.listadorazaperrosej8.data.remote.RazaAPI
+
+class Repositorio( private val razaAPI: RazaAPI, private val razaDao: RazaDao) {
+    suspend fun getRazas(){
+        val response = razaAPI.getData()// llegan los datos
+        if (response.isSuccessful){ //llegaron los datos?
+            val message = response.body()!!.message //solo sacando la parte del mensaje, sin status
+            val keys = message.keys
+
+            keys.forEach{
+                val razaEntity = RazaEntity(it)
+                razaDao.insertRaza(razaEntity)
+            }
+        }
+    }
+}
