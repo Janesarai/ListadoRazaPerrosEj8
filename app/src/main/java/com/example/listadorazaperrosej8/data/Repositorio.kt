14918db273e4e3b3fdf8a2afa7f6c85b73d9449d
Janesarai <saraipalaciosjane@gmail.com>
2023-08-10 19:28:50 -0400

    try para que no se caiga sin net

--- app/src/main/java/com/example/listadorazaperrosej8/data/Repositorio.kt
+++ app/src/main/java/com/example/listadorazaperrosej8/data/Repositorio.kt
@@ -12,32 +12,38 @@ class Repositorio( private val razaAPI: RazaAPI, private val razaDao: RazaDao) {
     fun obtenerRazasEntity(): LiveData<List<RazaEntity>> = razaDao.getRazas()
 
     fun obtenerRazaDetalle(id: String): LiveData<List<RazaDetalle>> = razaDao.getRazaDetalle(id)
-    suspend fun getRazas(){
-        val response = razaAPI.getData()// llegan los datos
-        if (response.isSuccessful){ //llegaron los datos?
-            val message = response.body()!!.message //solo sacando la parte del mensaje, sin status
-            val keys = message.keys
+    suspend fun getRazas() {
+        try {
+            val response = razaAPI.getData()// llegan los datos
+            if (response.isSuccessful) { //llegaron los datos?
+                val message =
+                    response.body()!!.message //solo sacando la parte del mensaje, sin status
+                val keys = message.keys
 
-            keys.forEach{
-                val razaEntity = RazaEntity(it)
-                razaDao.insertRaza(razaEntity)
-            }
-                }else{
-            Log.e("respositorio",response.errorBody().toString())
+                keys.forEach {
+                    val razaEntity = RazaEntity(it)
+                    razaDao.insertRaza(razaEntity)
                 }
+            }
+        } catch (exeption:Exception) {
+            Log.e("catch","")
         }
+    }
+
 
     suspend fun getDetallePerro(id:String){
+        // try para que no se caiga sin internet
+        try{
         val response = razaAPI.getDetalleRaza(id)// llegan los datos
-        if (response.isSuccessful){
-            response.body()!!.message.forEach{
-                val razaDetalle = RazaDetalle(id,it)
+        if (response.isSuccessful) {
+            response.body()!!.message.forEach {
+                val razaDetalle = RazaDetalle(id, it)
                 razaDao.insertDetallePerro(razaDetalle)  //seria el raza detalle que ed un entiti
             }
+        }
 
-
-            }else{
-                Log.e("respositorio",response.errorBody().toString())
+            }catch(exeption: Exception){
+                Log.e("catch","")
         }
     }
 }
